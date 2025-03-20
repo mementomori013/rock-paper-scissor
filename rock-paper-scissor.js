4a5aa3580c20bc61373b50573853649a1b15ce89
Memento mori <183689451+mementomori013@users.noreply.github.com>
2025-03-20 15:07:48 +0800

    Add UI to the rock paper scissor
    Create 3 buttons for rock paper and scissor
    Add functions

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,20 +1,59 @@
 
-let computerChoice = getComputerChoice();
-let userInput = prompt("0-scissor 1-rock 2-paper \n Input number: ");
-console.log("user input: " + userInput)
-let userInputRPS = getUserInput(parseInt(userInput));
 
-console.log("user: " + userInputRPS);
-console.log("computer: " + computerChoice);
+// let userInput = prompt("0-scissor 1-rock 2-paper \n Input number: ");
+// console.log("user input: " + userInput)
+// let userInputRPS = getUserInput(parseInt(userInput));
+// console.log("user: " + userInputRPS);
+// console.log("computer: " + computerChoice);
+const ul = document.querySelector("ul")
+const body = document.querySelector("body")
+const scoreHolder = document.createElement("div")
+const playersPick = document.createElement("p")
+const score = document.createElement("p")
+const winAnnounce = document.createElement("p")
+let playerPoints = 0;
+let computerPoints = 0;
 
 
-let result = game(userInputRPS, computerChoice);
-console.log (result)
+ul.addEventListener("click", (e)=>{
+        let playerPick = e.target.id
+        let computerChoice = getComputerChoice();
+        console.log(getUserInput(playerPick));
+        scoring(game(playerPick,computerChoice))
+        score.textContent = "player: " + playerPoints + " " + "computer: " + computerPoints
+        playersPick.textContent = "player: " + playerPick + "  " + "computer: " + computerChoice 
+        body.append(scoreHolder)
+        scoreHolder.append(playersPick)
+        scoreHolder.appendChild(score)    
+        if (playerPoints == 5) {
+            winAnnounce.textContent = "CONGRATULATIONS! YOU WIN!!!"
+            scoreHolder.appendChild(winAnnounce)
+            playerPoints = 0;
+            computerPoints = 0;
+        }
+        else if (computerPoints == 5) {
+            winAnnounce.textContent = "BETTER LUCK NEXT TIME. COMPUTER WIN!"
+            scoreHolder.appendChild(winAnnounce)
+            playerPoints = 0;
+            computerPoints = 0;
+        }
+        else if (playerPoints < 5 && computerPoints < 5) {
+            winAnnounce.textContent = ""
+        }
+         
+    })
+ 
+        
+
+
+
+//let result = game(userInputRPS, computerChoice);
+//console.log (game(userInputRPS, computerChoice));
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
 
-    console.log(num)
+    //console.log(num)
     switch (num) {
         case num = 0:
             return "scissor";
@@ -29,20 +68,19 @@ function getComputerChoice() {
             return "no" ;
             break;   
     };
-
     
     }
 
     function getUserInput(userInput1){
         switch (userInput1) {
-            case userInput= 0:
-                return "scissor";
-                break;
-            case userInput1 = 1:
+            case userInput = "rock":
                 return "rock";
+                break;
+            case userInput1 ="paper":
+                return "paper";
                 break;  
-            case userInput1 = 2:
-                return "paper";    
+            case userInput1 = "scissor":
+                return "scissor";    
                 break;
             default:
                 return "no" ;
@@ -55,11 +93,10 @@ function getComputerChoice() {
             let scissor = "scissor"
             let rock = "rock"
             let paper = "paper"
-            let win = "you won!"
-            let lose = "you lose!"
+            let win = "win"
+            let lose = "lose"
             let tie = "tie"
             if (user == scissor)  {
-                console.log("if" + scissor);
                 switch (computer) {
                     case computer = scissor:
                         return tie;
@@ -73,7 +110,6 @@ function getComputerChoice() {
                 }
             }
             if (user == rock)  {
-                console.log("if" + "rock");
                 switch (computer) {
                     case computer = rock:
                         return tie;
@@ -87,7 +123,6 @@ function getComputerChoice() {
                 }
             }
             if (user == paper) {
-                console.log("if" + "paper");
                 switch (computer) {
                     case computer = paper:
                         return tie;
@@ -100,4 +135,17 @@ function getComputerChoice() {
                         break;
                 }
             }
+}
+function scoring(result){
+    if (result == "win"){
+        playerPoints = playerPoints + 1 
+    }
+    else if (result == "lose"){
+        computerPoints = computerPoints + 1  
+    }
+    else if (result == "tie"){
+        playerPoints = playerPoints 
+        computerPoints = computerPoints 
+        
+    }
 }
